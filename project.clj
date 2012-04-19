a6013934ea6883e2dd254c0c14ff34ea4ff22fe0
ryan spangler <ryan.spangler@gmail.com>
2012-04-19 15:10:00 -0700

    broke unzip utilities out to their own project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject zippix "0.1.0"
+  :description "ZIPPIX:  Deal with zip files as resources from clojure"
+  :url "http://github.com/antler/caribou"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/zippix/core_test.clj
@@ -0,0 +1,7 @@
+(ns zippix.core-test
+  (:use clojure.test
+        zippix.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
